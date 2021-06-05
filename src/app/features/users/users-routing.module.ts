a03218ef9a099e310b49115827be7c18dd0e61d2
Wataru.Kasahara <kasahara1019@gmail.com>
2021-06-05 11:12:11 +0900

    refactor: Rename UsersComponent -> UsersPageComponent

--- src/app/features/users/users-routing.module.ts
+++ src/app/features/users/users-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersComponent } from './pages/users/users.component';
+import { UsersPageComponent } from './pages/users/users.component';
 
-const routes: Routes = [{ path: '', component: UsersComponent }];
+const routes: Routes = [{ path: '', component: UsersPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
